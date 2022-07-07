e5bcaa3a15fd38d145cde45dab27f03613c6d894
Abhishek Pandey <abhihits0404@gmail.com>
2022-07-08 00:54:54 +0530

    static page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import "./App.css";
-
+import Navbar from "./components/Navbar";
+import {Route,Routes} from "react-router-dom"
+import Home from "./components/Home";
+import Cart from "./components/Cart";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
   return (
     <>
-      <h1>hello pizza app</h1>
+     <Navbar />
+
+     <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+     </Routes>
+
     </>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,189 @@
+import React from "react";
+import { Input, useDisclosure } from "@chakra-ui/react";
+import { InputGroup } from "@chakra-ui/react";
+import { Link, useNavigate } from "react-router-dom";
+import { InputRightElement } from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
+import { MdShoppingCart } from "react-icons/md";
+import { GoTriangleDown } from "react-icons/go";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  Text,
+} from "@chakra-ui/react";
+
+import "./Navbar.css";
+// import { useDispatch, useSelector } from "react-redux";
+// import { logout } from "../store/auth/auth.action";
+// import { useEffect } from "react";
+const Navbar = () => {
+  // const {isAuth}=useSelector((state)=>state.auth.authenticate)
+  // const dispatch=useDispatch()
+  const navigate=useNavigate()
+  const data = [
+    {
+      name: "Flash Sale",
+      path: "/watches",
+    },
+    {
+      name: "Best Deals",
+      path: "/watches",
+    },
+    {
+      name: "Trending",
+      path: "/watches",
+    },
+    {
+      name: "New Arrivals",
+      path: "/watches",
+    },
+  ];
+
+  // const CategoriesData = [
+  //   {
+  //     heading: "Men",
+  //     list: [
+  //       { name: "Watches", path: "/watches" },
+  //       { name: "New Arrivals", path: "/watches" },
+  //     ],
+  //   },
+  //   {
+  //     heading: "Women",
+  //     list: [
+       
+  //       { name: "New Arrivals", path: "/watches" },
+  //       { name: "Watches", path: "/watches" },
+  //     ],
+  //   },
+  //   {
+  //     heading: "Electronics",
+  //     list: [
+  //       { name: "Mobile", path: "/watches" },
+  //       { name: "Electronics", path: "/watches" },
+  //       { name: "Wireless Chargers", path: "/watches" },
+  //       { name: "Wireless Speakers", path: "/watches" },
+  //     ],
+  //   },
+  //   {
+  //     heading: "KIDS & MOM",
+  //     list: [
+        
+  //       { name: "Water Bottles", path: "/watches" },
+  //       { name: "Toys", path: "/watches" },
+  //     ],
+  //   },
+  // ];
+  // const { isOpen, onOpen, onClose } = useDisclosure();
+  // const handleLogout=()=>{
+  //  dispatch(logout())
+  //  navigate("/login")
+  // }
+// useEffect(()=>{
+// if(!isAuth){
+//   navigate("/login")
+// }
+// },[isAuth])
+  return (
+    <div className="mainNav">
+      <div className="topNav">
+        <div className="leftTopNav">
+          <Link to="/" className="imgleftTopNav">
+            <img
+              src="https://cdn.dribbble.com/users/404971/screenshots/1241486/pizza-logo.png"
+              alt="logo"
+              style={{width:"100px", borderRadius:"50px",padding:"10px"}}
+            ></img>
+          </Link>
+
+          {/* <div className="inputDivTopNav">
+            <InputGroup>
+              <InputRightElement
+                pointerEvents="none"
+                children={<SearchIcon w={10} h={6} color="gray.300" />}
+              />
+              <Input type="text" placeholder="Search..." />
+            </InputGroup>
+          </div> */}
+        </div>
+        <div className="rightTopNav">
+         <div className="belowrightTopNav"><Button colorScheme="orange" >Logout</Button></div>
+         <div className="belowrightTopNav">
+            <div>
+              <Link to="/login">LOGIN</Link> / <Link to="/signup">SIGNUP</Link>
+            </div>
+
+            <div className="welcome">WELCOME GUEST</div>
+          </div>
+
+          <div className="carticon">
+           <Link to="/cart"> <MdShoppingCart size={40} /></Link>
+          </div>
+        </div>
+      </div>
+      <div className="bottomNav">
+        {/* <div className="leftBottomNav">
+          <Button onClick={onOpen}>Categories</Button>
+          <GoTriangleDown className="triangle" size={20} />
+          <Modal
+            blockScrollOnMount={false}
+            isOpen={isOpen}
+            size="4xl"
+            onClose={onClose}
+          >
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Categories</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                <Text fontWeight="bold" mb="1rem">
+                  <div className="CategoriesMain">
+                    {CategoriesData.map((e) => {
+                      return (
+                        <div key={e.heading}>
+                          <div className="CategoriesHeading">{e.heading}</div>
+                          <br />
+                          {e.list.map((s) => {
+                            return (
+                              <div className="footerList" key={s.name}>
+                                <Link to={s.path} onClick={onClose}>
+                                  {s.name}
+                                </Link>
+                              </div>
+                            );
+                          })}
+                        </div>
+                      );
+                    })}
+                  </div>
+                </Text>
+              </ModalBody>
+
+              <ModalFooter>
+                <Button colorScheme="blue" mr={3} onClick={onClose}>
+                  Close
+                </Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+        </div>
+        <div className="rightBottomNav">
+          {data.map((e) => {
+            return (
+              <div key={e.name}>
+                <Link to={e.path}>{e.name}</Link>
+              </div>
+            );
+          })}
+        </div> */}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar
